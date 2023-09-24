55074b544b11589863eb7077a09372e79c9a5ce7
Student20022 <131999657+Student20022@users.noreply.github.com>
2023-09-24 11:42:02 +0300

    Add files via upload

---
+++ Practica_ task1/Practica_ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введiть логiн: ");
+        string login = Console.ReadLine();
+
+        if (IsLoginValid(login))
+        {
+            Console.WriteLine("Логiн коректний.");
+        }
+        else
+        {
+            Console.WriteLine("Логiн некоректний.");
+        }
+    }
+
+    static bool IsLoginValid(string login)
+    {      
+        string pattern = @"^[a-zA-Z][a-zA-Z0-9]{1,9}$";
+
+        return Regex.IsMatch(login, pattern);
+    }
+}
---
+++ Practical_task2/Practical_task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть текст: ");
+        string inputText = Console.ReadLine();
+
+        string filteredText = FilterText(inputText);
+
+        Console.WriteLine("Результат:");
+        Console.WriteLine(filteredText);
+    }
+
+    static string FilterText(string inputText)
+    {
+        string pattern = @"\b(коректний|логін)\b";
+        string replacement = "валідний ім'я користувача";
+
+        string filteredText = Regex.Replace(inputText, pattern, replacement, RegexOptions.IgnoreCase);
+
+        return filteredText;
+    }
+}
